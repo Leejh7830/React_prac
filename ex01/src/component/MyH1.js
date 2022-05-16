bd94f3553f4b7e3643c81392e9c74e4e56705fb1
Lee JaeHyeong <leejh7830@naver.com>
2022-05-16 15:54:28 +0900

    220516
    react 기초

---
+++ ex01/src/component/MyH1.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const MyH1 = (props) => {
+
+    function doadd() {
+        setB(b+1);
+        // document.getElementById('b').innerHTML=b;
+        // console.log("증가"+b);
+    }
+    const dosub = ()=> {
+        setB(b-1);
+        // document.getElementById('b').innerHTML=b;
+        // console.log("감소"+b);
+    }
+    const getax = () => {
+        axios.get('https://dongjoonleedj.github.io/testWebHosting/myjson.json')
+        .then( (res) => { 
+            setA(res.data.Name);
+            setB(res.data.price);
+        } )
+        .catch( (error) => {console.log(error)} )
+    }
+    const [a,setA] = useState(props.aa);
+    const [b,setB] = useState(props.bb);
+    // var a = props.aa;
+    // var b = props.bb;
+  return (
+    <div>
+        <h1>MyH1</h1>
+        <p id="a">a = {a}</p>
+        <p id="b">b = {b}</p>
+        <button onClick={doadd}>b증가</button>
+        <button onClick={dosub}>b감소</button>
+        <button onClick={getax}>json 파일 가져오기</button>
+    </div>
+  )
+}
+
+export default MyH1
+
+// https://dongjoonleedj.github.io/testWebHosting/myjson.json
